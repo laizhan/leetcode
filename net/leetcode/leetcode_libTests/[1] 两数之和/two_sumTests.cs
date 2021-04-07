8e7427d6153515dfc93ede4aea62d2aa720203dc
lai zhan <laizhan@live.com>
2021-04-07 21:57:07 +0800

    添加测试用例

---
+++ net/leetcode/leetcode_libTests/[1] 两数之和/two_sumTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace leetcode_lib._1__两数之和.Tests
+{
+    [TestClass()]
+    public class two_sumTests
+    {
+        two_sum instance = new two_sum();
+
+        [TestMethod()]
+        public void TwoSumTest()
+        {
+            int[] test1 = new int[] { 2, 7, 11, 15 };
+            int[] test1TrueResult = new int[] { 0, 1 };
+            var test1Result = instance.TwoSum(test1, 9);
+            Assert.AreEqual(test1TrueResult.Sum(), test1Result.Sum());
+        }
+    }
+}
